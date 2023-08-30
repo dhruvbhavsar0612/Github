0e8bbe8a509228f7c8c7d06583be0349f78decc3
Bhavsar Dhruv Ajit <dhruvbhavsar9898@gmail.com>
2023-08-30 10:03:15 +0530

    setup

--- Text-Summarization-NLP-Project-main/setup.py
+++ Text-Summarization-NLP-Project-main/setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "dhruvbhavsar0612"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "dbhavsar9898@gmail.com"
 
 
 
